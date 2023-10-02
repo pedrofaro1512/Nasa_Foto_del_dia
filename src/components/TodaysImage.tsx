04f2c2c7ca2272d10425074b1abb9e49feddb476
Pedro Romero <108835699+pedrofaro1512@users.noreply.github.com>
2023-10-02 12:43:12 -0500

    Finalización de la app, adición de vista de detalle de cada imagn del dís

--- src/components/TodaysImage.tsx
+++ src/components/TodaysImage.tsx
@@ -1,15 +1,24 @@
 import React from "react";
 import { View, Text, StyleSheet, Image, Button } from "react-native";
-import { PostImage } from "../types";
+import { PostImage, RootStackParams } from "../types";
+import { useNavigation } from "@react-navigation/native";
+import { NativeStackNavigationProp } from "@react-navigation/native-stack";
 
-const TodaysImage: React.FC<PostImage> = ({ date, title, url }) => {
+type PostImageNavigationProps = NativeStackNavigationProp<RootStackParams, 'Detail'>
+
+const TodaysImage: React.FC<PostImage> = ({ date, title, url, explanation }) => {
+  const {navigate} = useNavigation<PostImageNavigationProps>();
+  const handleViewPress = () => {
+    navigate('Detail', {title, date, url, explanation});
+  };
+
   return (
     <View style={styles.container}>
       <Image source={{ uri: url }} style={styles.image} />
       <Text style={styles.title}>{title}</Text>
       <Text style={styles.date}>{date}</Text>
       <View style={styles.boton}>
-        <Button title="View" />
+        <Button title="View" onPress={handleViewPress} />
       </View>
     </View>
   );
--- src/views/Detail.tsx
+++ src/views/Detail.tsx
@@ -1,16 +1,65 @@
 import React from 'react';
-import { View, Text, StyleSheet } from 'react-native';
+import { View, Text, StyleSheet, Image, ScrollView } from 'react-native';
+import { useRoute } from '@react-navigation/native';
+import { NativeStackScreenProps } from '@react-navigation/native-stack';
+import { RootStackParams } from '../types';
+import Header from '../components/Header';
 
 const Detail = () => {
+  const { params: { title, url, explanation, date} } = useRoute<NativeStackScreenProps<RootStackParams, 'Detail'>['route']>();
   return (
-    <View>
-        <Text>Detail</Text>
+    <View style={styles.container}>
+      <Header />
+      <View style={styles.content}>
+        <Image source={{uri: url}} style={styles.image} />
+        <Text style={styles.title}>{title}</Text>
+        <Text style={styles.date}>{date}</Text>
+        <ScrollView style={styles.explanation}>
+          <Text style={styles.textExplanation}>{explanation}</Text>
+        </ScrollView>
+      </View>
     </View>
   )
 }
 
 const styles = StyleSheet.create({
-    
+    container: {
+      flex: 1,
+      backgroundColor: "rgba(7,26,93,255)",
+      paddingHorizontal: 10,
+    },
+    content: {
+      flex: 1,
+      backgroundColor: "#2c449d",
+      borderRadius: 32,
+      marginVertical: 16,
+      padding: 16
+    },
+    image: {
+      width: '100%',
+      height: '40%',
+      borderColor: "#FFF",
+      borderRadius: 32,
+      borderWidth: 2,
+      marginBottom: 16,
+    },
+    title: {
+      color: "#FFF",
+      fontSize: 22,
+      fontWeight: "bold",
+      marginBottom: 16,
+    },
+    date: {
+      color: "#FFF",
+      fontSize: 16
+    },
+    explanation: {
+      flex: 1,
+      marginVertical: 16,
+    },
+    textExplanation: {
+      color:"#FFF",
+    }
 })
 
 export default Detail;
